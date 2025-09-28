59bad9fe7d6ef6096c6aa9809ca4447f509ba2d5
Muhammed Ramazan Tuncak <muhammedtuncak@gmail.com>
2025-09-28 16:30:14 +0300

    Develop branch added

--- secret_notes.py
+++ secret_notes.py
@@ -1,4 +1,4 @@
-import binascii
+
 from tkinter import *
 from PIL import Image, ImageTk
 import pybase64
@@ -58,11 +58,7 @@ placer(password_entry,0.5,0.83)
 
 # password değerini alma
 
-value_list = []
-
-def get_password_value():
-    value = password_entry.get()
-    value_list.append(value)
+value_dict = {}
 
 # text oluşturma
 
@@ -82,36 +78,37 @@ def get_entries(argument):
 # click fonksiyonu oluşturma
 
 def encrypt_button():
-    secret = get_entries(note_text)
-    if get_entries(password_entry) == value_list[0]: # şifre için koruma sağlama
-        note_text.delete(1.0, END)  # text box temizleme
-        secret = secret.encode("utf-8") # text'teki yazıları byte tipine dönüştürme
-        secret = pybase64.b64encode(secret) # base 64'e çevirme
-        secret = secret.decode("utf-8")
-        note_text.insert(END,secret)
     if not get_entries(title_entry) or not get_entries(note_text) or not get_entries(password_entry):
-        messagebox.showwarning("Blank","You can not leave blank the relevant fields")
-        value_list.clear()
-    elif get_entries(password_entry) != value_list[0]:
-        messagebox.showwarning("Incorrect!", "Incorrect Password , Try Again")
-    else:
-        with open("top_secret.txt","a") as a:
-            a.write(f"YourTitle: {get_entries(title_entry)}\nYourSecretNote: {secret}\n\n")
+        messagebox.showwarning("Blank", "You can not leave blank the relevant fields")
+        return
+    secret = get_entries(note_text)
+    secret = secret.encode("utf-8") # text'teki yazıları byte tipine dönüştürme
+    secret = pybase64.b64encode(secret).decode("utf-8") # base 64'e çevirme
+    value = get_entries(password_entry)
+    value_dict[secret] = value
+    print(value_dict)
+    note_text.delete(1.0, END)  # text box temizleme
+    note_text.insert(END,secret)
+
+    with open("top_secret.txt","a") as a:
+        a.write(f"YourTitle: {get_entries(title_entry)}\nYourSecretNote: {secret}\n\n")
 
 def decrypt_button():
     try:
         secret = get_entries(note_text)
-        if get_entries(password_entry) == value_list[0]:
-            note_text.delete(1.0,END)
-            secret = secret.encode("utf-8")
-            secret = pybase64.b64decode(secret)
-            secret = secret.decode("utf-8")
-            note_text.insert(END, secret)
-        elif get_entries(password_entry) != value_list[0]:
-            messagebox.showwarning("Incorrect!", "Incorrect Password , Try Again")
+        if secret in value_dict:
+            if get_entries(password_entry) != value_dict[secret]:
+                messagebox.showwarning("Incorrect!", "Incorrect Password , Try Again")
+                return
+            elif get_entries(password_entry) == value_dict[secret]:
+                note_text.delete(1.0,END)
+                secret = secret.encode("utf-8")
+                secret = pybase64.b64decode(secret)
+                secret = secret.decode("utf-8")
+                note_text.insert(END, secret)
     except UnicodeError:
         messagebox.showerror("Attempts", "Too Many Attempts On Decrypt Button")
-    except binascii.Error:
+    except KeyError:
         messagebox.showerror("Error", "You Press Decrypt Button Before Encrypt The Text")
 
 def clear_button():
@@ -126,7 +123,7 @@ def button_maker(text,x,y,function):
     button_ex.config(text=text,width=20,command=function)
     placer(button_ex,x,y)
 
-button_maker("Save & Encrypt",0.5,0.89,lambda: [(get_password_value()),(encrypt_button()),(clear_button())])
+button_maker("Save & Encrypt",0.5,0.89,lambda: [(encrypt_button()),(clear_button())])
 button_maker("Decrypt",0.5,0.93,decrypt_button)
 button_maker("Clear",0.5,0.97,clear_button)
 
